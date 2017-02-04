d16609c8262c8a06e27c4dc434deae732a8cf2a8
Vagizova <vagizova.rita@gmail.com>
2017-02-04 20:22:53 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
 	$('.accordion').accordion({
 		collapsible: true,    
@@ -7,5 +8,21 @@ $(document).ready(function() {
 		collapsible: true,    
 		active: false,     
 	});
-	
+	   
+   $("#datepicker").datepicker({
+		monthNamesShort: [ "Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь" ],
+                changeMonth: true,
+
+	});
+  $("#datepicker table").addClass("datepicker__table");
+  // $( ".ui-datepicker-title" ).append( $( "h2" ) );
+  $( "<div class = 'arrow'></div>" ).appendTo( ".hasDatepicker" );
 });
+
+// $(document).ready(function() {
+	
+// });
+// $.datepicker.setDefaults(
+//         $.extend($.datepicker.regional["ru"])
+//   );
+
